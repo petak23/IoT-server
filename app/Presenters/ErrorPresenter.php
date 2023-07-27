9dd94af9bd939fafbff66ab2e76bcc8180e3c3ef
petak23-laptop <petak23@gmail.com>
2023-07-27 13:22:59 +0200

    29 - Oprava chyby.

--- app/Presenters/ErrorPresenter.php
+++ app/Presenters/ErrorPresenter.php
@@ -24,19 +24,19 @@ final class ErrorPresenter extends BasePresenter
 	}
 
 
-	public function run(Nette\Application\Request $request): Nette\Application\IResponse
+	public function run(Nette\Application\Request $request): Nette\Application\Response
 	{
 		$exception = $request->getParameter('exception');
 
 		if ($exception instanceof Nette\Application\BadRequestException) {
-			[$module, , $sep] = Nette\Application\Helpers::splitName($request->getPresenterName());
+			[$module,, $sep] = Nette\Application\Helpers::splitName($request->getPresenterName());
 			return new Responses\ForwardResponse($request->setPresenterName($module . $sep . 'Error4xx'));
 		}
 
 		$this->logger->log($exception, ILogger::EXCEPTION);
 		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
 			if (preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'))) {
-				require __DIR__ . '/templates/Error/500.phtml';
+				require __DIR__ . '/../templates/Error/500.phtml';
 			}
 		});
 	}
