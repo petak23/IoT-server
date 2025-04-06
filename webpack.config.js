ffb97f95db3c08d27aeda8b5c80f0e6c48e090f0
petak23 <petak23@gmail.com>
2025-04-06 21:17:50 +0200

    32 - Updates 2

--- webpack.config.js
+++ webpack.config.js
@@ -12,13 +12,17 @@ const devMode = process.env.NODE_ENV !== "production";
 const VUE_VERSION = require("vue/package.json").version;
 const VUE_LOADER_VERSION = require("vue-loader/package.json").version;
 
+// Config
+const ROOT_PATH = __dirname;
+const CACHE_PATH = ROOT_PATH + "/temp/webpack";
+
 // Webpack plugins
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const {VueLoaderPlugin} = require("vue-loader");
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 // Webpack abilities
 const WEBPACK_DEV_SERVER_HOST = process.env.WEBPACK_DEV_SERVER_HOST || 'localhost';
@@ -26,27 +30,21 @@ const WEBPACK_DEV_SERVER_PORT = parseInt(process.env.WEBPACK_DEV_SERVER_PORT, 10
 const WEBPACK_DEV_SERVER_PROXY_HOST = process.env.WEBPACK_DEV_SERVER_PROXY_HOST || 'localhost';
 const WEBPACK_DEV_SERVER_PROXY_PORT = parseInt(process.env.WEBPACK_DEV_SERVER_PROXY_PORT, 10) || 8000;
 
-// Config
-const ROOT_PATH = __dirname;
-const CACHE_PATH = ROOT_PATH + "/temp/webpack";
-
-var AssetsPlugin = require('assets-webpack-plugin');
-
 module.exports = {
-  mode: devMode ? "development" : "production",
-  context: path.join(ROOT_PATH, "app/assets"),
-  entry: {
-    front: path.join(ROOT_PATH, "app/assets/js/main.js"),
-  },
-  output: {
-    path: path.join(ROOT_PATH, 'www/dist'),
-    publicPath: "",
-    filename: devMode ? '[name].bundle.js' : '[name].[chunkhash:8].bundle.js',
-    clean: true,
-    //chunkFilename: devMode ? '[name].chunk.js' : '[name].[chunkhash:8].chunk.js'
-  },
-  devtool: 'cheap-module-source-map',
-  module: {
+	mode: devMode ? "development" : "production",
+	context: path.join(ROOT_PATH, "app/assets"),
+	entry: {
+		front: path.join(ROOT_PATH, "app/assets/front/js/main.js"),
+		admin: path.join(ROOT_PATH, "app/assets/admin/js/main.js")
+	},
+	output: {
+		path: path.join(ROOT_PATH, 'www/dist'),
+		publicPath: "",
+		filename: devMode ? '[name].bundle.js' : '[name].[chunkhash:8].bundle.js',
+		clean: true,
+	},
+	devtool: 'cheap-module-source-map',
+	module: {
 		noParse: /^(vue|vue-router|vuex|vuex-router-sync)$/,
 		rules: [
 			{
@@ -84,51 +82,58 @@ module.exports = {
 			},*/
 			{
 				test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
-				use: [
-					{
-						loader: 'url-loader',
-						options: {
-							limit: 4096,
-							fallback: {
-								loader: 'file-loader',
-								options: {
-									name: 'fonts/[name].[hash:8].[ext]'
-								}
-							}
-						}
-					}
-				]
+				type: 'asset/resource',
+				generator: {
+					filename: 'fonts/[hash][ext][query]'
+				},
 			},
 			{
 				test: /\.(svg)(\?.*)?$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: 'imgs/[name].[hash:8].[ext]'
-						}
-					}
-				]
+				type: 'asset/resource',
+				generator: {
+					filename: 'imgs/[hash:5][ext][query]'
+				},
 			},
 			{
 				test: /\.(png|jpe?g|gif|webp|ico)(\?.*)?$/,
+				type: 'asset/resource',
+				generator: {
+					filename: 'imgs/[hash:5][ext][query]'
+				},
+			},
+			/*{
+				test: /\.s[ac]ss$/i,
 				use: [
+					MiniCssExtractPlugin.loader,
+				  //"style-loader",
 					{
-						loader: 'url-loader',
+						loader: 'css-loader',
 						options: {
-							limit: 4096,
-							fallback: {
-								loader: 'file-loader',
-								options: {
-									name: 'imgs/[name].[hash:8].[ext]'
-								}
+							sourceMap: false,
+							importLoaders: 2,
+							modules: false
+						}
+					},
+					{
+						loader: "postcss-loader",
+						options: {
+							postcssOptions: {
+								ident: "postcss",
+								plugins: [require("autoprefixer")]
 							}
 						}
-					}
-				]
-			},
-			{
-				test: /\.(css|scss)$/,
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							// Prefer `dart-sass`, even if `sass-embedded` is available
+							implementation: require("sass"),
+						},
+					},
+				],
+			},*/
+			/*{
+				test: /\.(css|scss|sass)$/,
 				use: [
 					MiniCssExtractPlugin.loader,
 					{
@@ -148,16 +153,18 @@ module.exports = {
 							}
 						}
 					},
-          {
-            loader: 'sass-loader',
-            //options: {
-              // This is the path to your variables
-              //additionalData: "@import '@/admin/css/scss/variables.scss';"
-            //},
-          },
+					{
+						loader: 'sass-loader',
+						options: {
+							//api: "modern-compiler",
+							implementation: require('sass'),
+							// This is the path to your variables
+							//additionalData: "@import '@/front/css/scss/variables.scss';"
+						},
+					},
 				],
-			},
-      {
+			},*/
+			/*{
 				test: /\.sass$/,
 				use: [
 					MiniCssExtractPlugin.loader,
@@ -178,52 +185,73 @@ module.exports = {
 							}
 						}
 					},
-          {
-            loader: 'sass-loader',
-            options: {
-              // This is the path to your variables
-              additionalData: "@import '@/admin/css/scss/variables.scss'"
-            },
-          },
+					{
+						loader: 'sass-loader',
+						options: {
+							implementation: require('sass'),
+							// This is the path to your variables
+							//additionalData: "@import '@/front/css/scss/variables.scss'"
+						},
+					},
 				],
-			},
+			},*/
+			{
+				test: /\.(css|scss|sass)$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: false, // Vypnuté zdrojové mapy pre produkciu, zapni ich, ak sú potrebné
+							importLoaders: 2, // Umožňuje načítanie predchádzajúcich loaderov
+							modules: false, // Ak nepoužívaš CSS Modules, nechaj false
+						},
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								ident: 'postcss',
+								plugins: [require('autoprefixer')], // Autoprefixer pre kompatibilitu s prehliadačmi
+							},
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							implementation: require('sass'), // Používať `dart-sass`
+							sourceMap: false, // Môžeš zapnúť pre debugovanie
+							additionalData: `
+								@use "@/front/css/scss/variables.scss" as *; 
+							`,// Globálne premenné
+						},
+					},
+				],
+			},			
 		]
 	}, 
-  resolve: {
-    alias: {
-        'vue$': 'vue/dist/vue.esm.js',
-        '@': path.resolve(ROOT_PATH, 'app/assets'),
-    },
-    extensions: ['.js', '.vue']
-  },
-  plugins: [
-    // enable vue-loader to use existing loader rules for other module types
+	resolve: {
+		alias: {
+				'vue$': 'vue/dist/vue.runtime.esm-bundler.js',//https://stackoverflow.com/questions/65254050/after-upgrading-to-vue-3-and-updating-vue-instantiation-to-createapp-webpack-b
+				'@': path.resolve(ROOT_PATH, 'app/assets'),
+		},
+		extensions: ['.js', '.vue']
+	},
+	plugins: [
+		// enable vue-loader to use existing loader rules for other module types
 		new VueLoaderPlugin(),
 
-    new VuetifyLoaderPlugin(),
-    
-    // fix legacy jQuery plugins which depend on globals
-		new webpack.ProvidePlugin({
-			$: "jquery",
-			jQuery: "jquery",
-			"window.jQuery": "jquery",
-			"window.$": "jquery",
-      Popper: ["popper.js", "default"],
-      naja: ['naja', 'default'],  // https://forum.nette.org/cs/25444-ublaboo-datagrid-mocny-rychly-rozsiritelny-hezky-anglicky-dokumentovany-datagrid?p=36#p213906
+		//new VuetifyLoaderPlugin(),
+		
+		new MiniCssExtractPlugin({
+			filename: devMode ? '[name].bundle.css' : '[name].[chunkhash:8].bundle.css'
 		}),
-    
-    new MiniCssExtractPlugin({
-      filename: devMode ? '[name].bundle.css' : '[name].[chunkhash:8].bundle.css'
-    }),
-    new AssetsPlugin({ // Pre aplikaciu filename: '[name].[contenthash:8].[ext]' a prepojenie s nette
-      //includeManifest: 'manifest',
-      path: path.join(ROOT_PATH, 'www/dist')
-    })
-  ],
-  //devtool: 'cheap-module-source-map',
-  performance: {
-    hints: false
-  }
+		new WebpackManifestPlugin()
+	],
+	//devtool: 'cheap-module-source-map',
+	performance: {
+		hints: false
+	}
 };
 
 
@@ -232,26 +260,26 @@ module.exports = {
 // ****************************
 
 if (process.env.NODE_ENV === 'development') {
-  const development = {
-    devServer: {
-      host: WEBPACK_DEV_SERVER_HOST,
-      port: WEBPACK_DEV_SERVER_PORT,
-      //disableHostCheck: true,
-      //contentBase: path.join(ROOT_PATH, 'www'),
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': '*'
-      },
-      //stats: 'errors-only',
-      hot: true,
-      //inline: true,
-      proxy: {
-        '/': `http://${WEBPACK_DEV_SERVER_PROXY_HOST}:${WEBPACK_DEV_SERVER_PROXY_PORT}`
-      }
-    }
-  };
+	const development = {
+		devServer: {
+			host: WEBPACK_DEV_SERVER_HOST,
+			port: WEBPACK_DEV_SERVER_PORT,
+			//disableHostCheck: true,
+			//contentBase: path.join(ROOT_PATH, 'www'),
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Headers': '*'
+			},
+			//stats: 'errors-only',
+			hot: true,
+			//inline: true,
+			proxy: {
+				'/': `http://${WEBPACK_DEV_SERVER_PROXY_HOST}:${WEBPACK_DEV_SERVER_PROXY_PORT}`
+			}
+		}
+	};
 
-  module.exports = merge(module.exports, development);
+	module.exports = merge(module.exports, development);
 }
 
 
@@ -260,20 +288,20 @@ if (process.env.NODE_ENV === 'development') {
 // ***************************
 
 if (process.env.NODE_ENV === 'production') {
-  const production = {
-    devtool: 'source-map',
-    optimization: {
-      minimizer: [
-        new TerserPlugin({
+	const production = {
+		devtool: 'source-map',
+		optimization: {
+			minimizer: [
+				new TerserPlugin({
 					test: /\.m?js(\?.*)?$/i,
 				}),
-        new CssMinimizerPlugin(),
-      ],
-      minimize: true,
-    },
-  };
+				new CssMinimizerPlugin(),
+			],
+			minimize: true,
+		},
+	};
 
-  module.exports = merge(module.exports, production);
+	module.exports = merge(module.exports, production);
 }
 
 
@@ -282,10 +310,10 @@ if (process.env.NODE_ENV === 'production') {
 // ************************
 
 if (process.env.WEBPACK_REPORT === '1') {
-  module.exports.plugins.push(
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'server',
-      openAnalyzer: true,
-    })
-  );
+	module.exports.plugins.push(
+		new BundleAnalyzerPlugin({
+			analyzerMode: 'server',
+			openAnalyzer: true,
+		})
+	);
 }
